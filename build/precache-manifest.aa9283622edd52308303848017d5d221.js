75e9b936205cebdaf1182038de255cf92f7fd2ca
guneet96 <gsgsdhillon@gmail.com>
2020-06-14 19:43:18 -0300

    init

---
+++ build/precache-manifest.aa9283622edd52308303848017d5d221.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da0ee26d0997a299eb31119b94565d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298334bd7773b77cd214",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "a66877078eb02feae5a2",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "298334bd7773b77cd214",
+    "url": "/static/js/2.69b1aab3.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.69b1aab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66877078eb02feae5a2",
+    "url": "/static/js/main.f2aa62df.chunk.js"
+  },
+  {
+    "revision": "c68ed30293225d45fc32",
+    "url": "/static/js/runtime-main.5b599322.js"
+  },
+  {
+    "revision": "5eade4fa6851f16d8d653d4ffe1765d3",
+    "url": "/static/media/baskets.5eade4fa.jpeg"
+  },
+  {
+    "revision": "a0533e6724ec5b8fe9e02d5948bd44d3",
+    "url": "/static/media/chair.a0533e67.jpeg"
+  },
+  {
+    "revision": "5cce32ac00c91e189e1cb94d7c9a8090",
+    "url": "/static/media/glasses.5cce32ac.jpeg"
+  },
+  {
+    "revision": "0782bdf945f8a7ed69515a5c01a794d7",
+    "url": "/static/media/halifax_rural.0782bdf9.jpeg"
+  },
+  {
+    "revision": "486678f5d4c2351855fe4bb62027af37",
+    "url": "/static/media/halifax_urban.486678f5.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aa8dd5c402ef139d706f5f60daf28980",
+    "url": "/static/media/logo.aa8dd5c4.jpeg"
+  },
+  {
+    "revision": "36612091873990b2d7d7311c1fe50a63",
+    "url": "/static/media/map.36612091.jpeg"
+  },
+  {
+    "revision": "cec085665fac0d2bcfa8345257e917bf",
+    "url": "/static/media/nightstand.cec08566.jpeg"
+  },
+  {
+    "revision": "6f1ab8512f1914dadcd5e4a642aa33b9",
+    "url": "/static/media/pillow.6f1ab851.jpeg"
+  },
+  {
+    "revision": "2b4490632013750cbcb3749ac5f1c11f",
+    "url": "/static/media/sell.2b449063.jpeg"
+  },
+  {
+    "revision": "d77f2493d6b15e0099febcc6ad0ff87a",
+    "url": "/static/media/user.d77f2493.jpeg"
+  }
+]);
